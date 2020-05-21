9154b155ed76a09310068f3a387afcc007437ff7
darshikasingh <65711746+darshikasingh@users.noreply.github.com>
2020-05-21 16:31:44 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,8 @@
+a = int(input("ENTER THE VALUE OF a: "))
+b = int(input("ENTER THE VALUE OF b: "))
+a = a+b
+b = a-b
+a = a-b
+print("AFTER SWAPPING")
+print("VALUE OF a =", a)
+print("VALUE OF b =", b)
