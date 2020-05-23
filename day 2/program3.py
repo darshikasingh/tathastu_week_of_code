c9143fb5eadd88a093b772a8e756f102bd39d7ce
darshikasingh <65711746+darshikasingh@users.noreply.github.com>
2020-05-23 14:40:02 +0530

    Create program3.py

---
+++ day 2/program3.py
@@ -0,0 +1,27 @@
+n=int(input("ENTER THE VALUE OF N: "))
+for i in range(n):
+     print(" " * i + "*" + " " * (n - 1 - i) + "*")
+for i in range(n):
+     print(" " * (n - 1 - i) + "*" + " " * i + "*")
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
